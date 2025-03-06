8ebae69f32d2a1ddb16fdd63f4862a27496dfbbc
Truongle68 <truonglephuc11@gmail.com>
2025-03-07 00:56:58 +0700

    init FE, BE auth routes

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signUpController = (req, res) => {
+    res.send('Sign up');
+}
+
+export const loginController = (req, res) => {
+    res.send('Login');
+}
+
+export const logoutController = (req, res) => {
+    res.send('Logout');
+}
---
+++ backend/src/routes/auth.route.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { loginController, logoutController, signUpController } from '../controllers/auth.controller.js';
+
+const router = express.Router();
+
+router.post('/signup', signUpController)
+
+router.post('/login', loginController)
+
+router.get('/logout', logoutController)
+
+export default router;  

c560a711a48e6fa1378b93a87913db317791615d
Truongle68 <truonglephuc11@gmail.com>
2025-03-07 01:35:04 +0700

    config mongodb

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,16 @@
 import express from 'express';
-import authRoutes from './routes/auth.route.js';
+import dotenv from 'dotenv'
+import authRoutes from './routes/auth.route.js'; 
+import { connectDB } from './lib/db.js';
+
+dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 4000;
 
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
+    connectDB();
 })
---
+++ backend/src/lib/db.js
@@ -0,0 +1,12 @@
+import mongooses from 'mongoose';
+
+export const connectDB = async () => {
+    try {
+        const conn = await mongooses.connect(process.env.MONGO_URI, {
+
+        })
+        console.log(`MongoDB connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.log("MongoDB connection error: ", error);
+    }
+}

cf01a18a667a2e248cc3b7bc1d0ef020d7029a6b
Truongle68 <truonglephuc11@gmail.com>
2025-03-15 12:04:12 +0700

    Fix cors block

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,14 +4,22 @@ import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js'; 
 import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
+import cors from 'cors'
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+
 app.use(express.json())
 app.use(cookieParser())
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+        credentials: true
+    })
+)
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);

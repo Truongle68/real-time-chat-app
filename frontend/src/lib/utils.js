0d27162938279e9e6ee2fd05ba1d39292890cebc
Truongle68 <truonglephuc11@gmail.com>
2025-03-26 17:33:13 +0700

    Refactor formatMessageDate function

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -1,40 +1,40 @@
-export function formatMessageDate(date) {
-  const timeFormat = new Date(date).toLocaleTimeString("en-US", {
+export function formatMessageDate(dateString) {
+  const date = new Date(dateString);
+  const timeFormat = date.toLocaleTimeString("en-US", {
     hour: "2-digit",
     minute: "2-digit",
     hour12: false,
   });
-
   // Check if the date is today
-  if (isToday(new Date(date))) {
+  if (isToday(date)) {
     return timeFormat;
   }
-  const dayAndTimeFormat = new Date(date).toLocaleDateString("en-US", {
-    weekday: "long",
-  });
-
   // Check if the date is this week
-  if (isThisWeek(new Date(date))) {
-    return `${dayAndTimeFormat} ${timeFormat}`;
-  } else {
-    // Format the date to "MM:DD:YYYY" if not this week
-    const options = { year: "numeric", month: "2-digit", day: "2-digit" };
-    const dateFormat = new Date(date).toLocaleDateString("en-US", options);
-    return `${timeFormat} ${dateFormat}`;
+  if (isThisWeek(date)) {
+    const weekday = date.toLocaleDateString("en-US", {
+      weekday: "long",
+    });
+    return `${weekday} ${timeFormat}`;
   }
+  // Format the date to "MM:DD:YYYY" if not this week
+  const dateFormat = date.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+  });
+  return `${timeFormat} ${dateFormat}`;
 }
 
 function isToday(date) {
   const today = new Date();
-  return (
-    date.getDate() === today.getDate() &&
-    date.getMonth() === today.getMonth() &&
-    date.getFullYear() === today.getFullYear()
-  );
+  return date.toDateString() === today.toDateString()
 }
 
 function isThisWeek(date) {
   const today = new Date();
-  const startOfWeek = new Date(today.getDate() - (today.getDay() || 7) + 1);
+  const startOfWeek = new Date(today)
+  // Get the Monday of the current week
+  startOfWeek.setDate(today.getDate() - (today.getDay() || 7) + 1)
+  startOfWeek.setHours(0,0,0,0)
   return date >= startOfWeek && date <= today;
 }
